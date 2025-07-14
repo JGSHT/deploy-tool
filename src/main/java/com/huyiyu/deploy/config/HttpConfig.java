097ab4e350c934c50a1ebe8a67c6ce4df42d4114
胡毅宇 <huyiyudesigner@163.com>
2025-07-14 21:29:35 +0800

    解决反射问题

--- src/main/java/com/huyiyu/deploy/config/HttpConfig.java
+++ src/main/java/com/huyiyu/deploy/config/HttpConfig.java
@@ -3,21 +3,21 @@ package com.huyiyu.deploy.config;
 import com.huyiyu.deploy.http.ArtifactoryExchange;
 import com.huyiyu.deploy.property.DeployProperties;
 import com.huyiyu.deploy.property.DeployProperties.Repo;
-import java.nio.charset.StandardCharsets;
-import java.time.Duration;
-import java.util.Base64;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Base64;
 
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(DeployProperties.class)
@@ -31,7 +31,7 @@ public class HttpConfig {
         .build();
     HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
         build);
-    requestFactory.setReadTimeout(Duration.ofSeconds(3));
+    requestFactory.setReadTimeout(Duration.ofSeconds(60));
     requestFactory.setConnectionRequestTimeout(Duration.ofSeconds(5));
     requestFactory.setConnectTimeout(Duration.ofSeconds(10));
     return RestClient.builder().requestFactory(requestFactory);
--- src/main/java/com/huyiyu/deploy/http/ArtifactoryExchange.java
+++ src/main/java/com/huyiyu/deploy/http/ArtifactoryExchange.java
@@ -1,10 +1,15 @@
 package com.huyiyu.deploy.http;
 
+import org.springframework.aot.hint.annotation.Reflective;
 import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
+@Reflective
 @HttpExchange
 public interface ArtifactoryExchange {
 
-  Resource pull(String s);
+  @GetExchange
+  Resource pull(@PathVariable("/{version}") String version);
 }
